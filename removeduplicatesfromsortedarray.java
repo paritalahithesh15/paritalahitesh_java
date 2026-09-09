fa8c6af92c3ab1edc7765be0e80f3761f4cbd4b9
Hitesh Paritala <paritalahithesh@gmail.com>
2026-09-09 12:23:03 +0530

    Implement removeDuplicates method in Solution class

---
+++ removeduplicatesfromsortedarray.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+
+        int k = 1;
+        int i = 1;
+
+        while (i < nums.length) {
+
+            if (nums[i] != nums[i - 1]) {
+                nums[k] = nums[i];
+                k++;
+            }
+
+            i++;
+        }
+
+        return k;
+    }
+}
